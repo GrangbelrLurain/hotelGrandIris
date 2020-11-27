cac398a4fd02ea25ff3cd55a232950a1b4fedd12
GrangbelrLurain <pcaso5@naver.com>
2020-11-27 22:51:20 +0900

    main_offer update

--- assets/js/offer.js
+++ assets/js/offer.js
@@ -2,11 +2,11 @@
 loadingToday();
 
 function loadingToday(){
-  const today = new Date();
-  const nextday = new Date(today.setDate(today.getDate() + 1));
+  const today = new Date(Date.now());
+  const nextday = new Date(Date.now() + 86400000)
   const dates = [today.getFullYear() + "년" + (today.getMonth() + 1) + "월" + today.getDate() + "일",
   (nextday.getFullYear()) + "년" + (nextday.getMonth() + 1) + "월" + nextday.getDate() + "일"];
-  console.log(dates[1]);
+  
 
   document.querySelector(".dateSelect").innerHTML = dates[0] + " ~ " + dates[1];
 }
