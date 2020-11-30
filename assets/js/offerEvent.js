131aec97a23ed2dde5739e70120ad232666efe50
GrangbelrLurain <pcaso5@naver.com>
2020-11-30 17:47:32 +0900

    main_ offer event update

--- assets/js/offerEvent.js
+++ assets/js/offerEvent.js
@@ -11,4 +11,32 @@ function loadingToday(){
   document.querySelector(".dateSelect").innerHTML = dates[0] + " ~ " + dates[1];
 }
 
-document.querySelector("")
+// ROOM 선택 클릭 이벤트 추가
+document.querySelectorAll(".offer .roomSelectWrap .roomSelect ul li").forEach((elem) => {
+
+  const btnElem = document.querySelector(".offer .roomSelectWrap .roomSelectBtn");
+  const allElem = document.querySelectorAll(".offer .roomSelectWrap .roomSelect ul li");
+  const clickElem = elem;
+  const clickElemName = elem.innerHTML;
+
+  elem.addEventListener("click",() => {
+    btnElem.innerHTML = clickElemName;
+    allElem.forEach((elem) => elem.classList.remove("active"));
+    clickElem.classList.add("active");
+  })
+});
+
+// ETC : ROOM, ADULT, CHILD (개수) 클릭 이벤트 추가
+document.querySelectorAll(".offer .etcOption ul").forEach((elem, index) => {
+  const clickGroupIndex = index;
+  const clickGroupAllElem = elem.querySelectorAll("li");
+  const btnElem = document.querySelectorAll(".etcSelectWrap > div > button")[clickGroupIndex];
+  clickGroupAllElem.forEach((elem) => {
+    const clickElem = elem;
+    elem.addEventListener("click", () => {
+      clickGroupAllElem.forEach((elem) => {elem.classList.remove("active")});
+      clickElem.classList.add("active");
+      btnElem.innerHTML = clickElem.innerHTML;
+    })
+  })
+})
