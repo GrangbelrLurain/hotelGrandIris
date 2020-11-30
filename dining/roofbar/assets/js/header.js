d5a947dbcff51a0a62c6fbd597f251c96c01e199
GrangbelrLurain <pcaso5@naver.com>
2020-11-30 17:47:45 +0900

    dining sub2 page update

---
+++ dining/roofbar/assets/js/header.js
@@ -0,0 +1,20 @@
+$(function() {
+$(document).mouseup(function(e) {
+    if ($(e.target).parents('.menu').length == 0) {
+        $('.menu li').removeClass('expand');
+        $('.menu ul').hide();
+    }
+});
+
+
+$(".menu li>a").click(function() {
+    var li = $(this).parent();
+    var ul = li.parent()
+    ul.find('li').removeClass('expand');
+    ul.find('ul').not(li.find('ul')).hide();
+    li.children('ul').toggle();
+    if (li.children('ul').is(':visible') || li.has('ul')) {
+        li.addClass('expand');
+    }
+});
+});
