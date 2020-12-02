fcdb1bd43b8adf11110ad163d6b92eab375b0a04
GrangbelrLurain <pcaso5@naver.com>
2020-12-02 14:23:09 +0900

    wedding change2

--- wedding/assets/js/footer.js
+++ wedding/assets/js/footer.js
@@ -1,3 +1,3 @@
-document.querySelector("#family").addEventListener("change", function(){
+document.querySelector("#family").addEventListener("onchange",function(){
   if(this.value) window.open(this.value)
 });
